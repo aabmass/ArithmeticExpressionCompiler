7059ae1713cd3266bdd3ff20d8c09425f9e3bead
Aaron Abbott <aabmass@gmail.com>
2016-01-09 13:52:42 -0500

    Add a main.cpp
    Just so cmake can compile right now

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) { 
+    std::cout << "This is just something for CMake to compile into an exexutable"
+              << std::endl;
+
+    return 0;
+}
