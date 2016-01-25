9a9f80853a565c0964789518903ecdba423a8a8b
Aaron Abbott <aabmass@gmail.com>
2016-01-25 13:04:45 -0500

    Branched out token class

---
+++ src/token.cpp
@@ -0,0 +1,10 @@
+#include "token.hpp"
+
+namespace AEC {
+    Token::Token(std::string const& value, std::string const& classification) :
+        value(value),
+        classification(classification)
+    {
+
+    }
+}
--- src/token.hpp
+++ src/token.hpp
@@ -1,9 +1,16 @@
 #ifndef TOKEN_HPP_DDF1MEPC
 #define TOKEN_HPP_DDF1MEPC
 
+#include <string>
+
 namespace AEC {
     class Token {
+    private:
+        std::string value;
+        std::string classification;
 
+    public:
+        Token(std::string const& value, std::string const& classification);
     };
 }
 
